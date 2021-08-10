6fae886ba7defd49b9596019505fe69d190bfa53
davidcorrado <davidcorrado@gmail.com>
2021-08-09 22:21:45 -0500

    Cleanup Files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
         jvmTarget = "11"
@@ -41,7 +41,6 @@ android {
         kotlinCompilerExtensionVersion = "1.0.1"
     }
     buildFeatures {
-        viewBinding = true
         compose = true
     }
 }
